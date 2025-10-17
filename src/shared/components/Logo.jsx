42bfd4c8f1652b5f00c6bb6f1b06281bf2c61b07
newneoshin <newneoshin@posteo.com>
2025-10-17 17:35:22 +0900

    [FEAT/#1] configure Logo

---
+++ src/shared/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="bg-red-700 p-8 rounded-md">
+      <p className="text-white text-3xl font-extrabold">SHIA SAYS</p>
+    </div>
+  );
+};
+
+export default Logo;
