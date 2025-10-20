929201b49798cc8d3c7dcf15af944dbaca6bd7fe
newneoshin <newneoshin@posteo.com>
2025-10-20 09:22:27 +0900

    [FEAT/#2] modifiy SquareButton design

--- src/shared/components/SquareButton.jsx
+++ src/shared/components/SquareButton.jsx
@@ -1,6 +1,8 @@
-const SquareButton = ({ text, onClick }) => {
+const SquareButton = ({ type = "primary", text, onClick }) => {
+  const color = type === "other" ? "bg-gray-500" : "bg-red-700";
+
   return (
-    <button className="bg-red-700 p-4 rounded-md" onClick={onClick}>
+    <button className={`${color} p-4 rounded-md w-full`} onClick={onClick}>
       <p className="text-white text-xl">{text}</p>
     </button>
   );
