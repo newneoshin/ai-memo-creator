54cd2963eeb8407e25154be60c77c81c39dccf8a
newneoshin <newneoshin@posteo.com>
2025-10-20 14:56:55 +0900

    [REF/#11] modify authSlice
    add AsyncThunk

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,23 +1,49 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authAPI from "./authAPI.js";
 
 const initialState = {
   token: null,
   error: null,
 };
 
+const signUp = createAsyncThunk(
+  "auth/signup",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const data = await authAPI.signUp({ email, password });
+      return data.access_token;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
+const login = createAsyncThunk(
+  "auth/login",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const data = await authAPI.login({ email, password });
+      return data.access_token;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {
-    setToken: (state, action) => {
-      state.token = action.payload;
-    },
-    resetToken: (state) => {
-      state.token = null;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        state.token = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.error = action.payload;
+      });
   },
 });
 
-export const { setToken, resetToken } = authSlice.actions;
-
+export { signUp, login };
 export default authSlice.reducer;
