abc852274d406ba903aedbea3ab58b8e9e8bd09f
newneoshin <newneoshin@posteo.com>
2025-10-20 16:23:03 +0900

    [REF/#13] apply Bar & List

--- src/pages/TodosPage.jsx
+++ src/pages/TodosPage.jsx
@@ -1,5 +1,37 @@
+import { useState, useMemo } from "react";
+import Logo from "../shared/components/Logo";
+import TodoList from "../features/todos/components/TodoList";
+import ListFilterBar from "../features/todos/components/ListFilterBar";
+
 const TodosPage = () => {
-  return <div>TODOS PAGE</div>;
+  const todos = [
+    { id: 1, title: "title1", content: "good!", isCompleted: false },
+    { id: 2, title: "title2", content: "good!!", isCompleted: true },
+    { id: 3, title: "title3", content: "good!!!", isCompleted: true },
+    { id: 4, title: "title4", content: "good!!!!", isCompleted: false },
+  ];
+  const [order, setOrder] = useState("all");
+  const filteredTodos = useMemo(() => {
+    if (order === "incomplete") {
+      return todos.filter((item) => {
+        return item.isCompleted === false;
+      });
+    } else if (order === "complete") {
+      return todos.filter((item) => {
+        return item.isCompleted === true;
+      });
+    } else {
+      return todos;
+    }
+  }, [order]);
+
+  return (
+    <div>
+      <Logo />
+      <ListFilterBar onChange={setOrder} />
+      <TodoList todos={filteredTodos} />
+    </div>
+  );
 };
 
 export default TodosPage;
